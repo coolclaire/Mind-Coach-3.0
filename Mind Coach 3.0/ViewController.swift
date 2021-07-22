5053af1c1690d5512323d0827ebf821c4d15503d
Test Name <test@gmail.com>
2021-07-21 23:15:38 -0700

    Initial Commit

---
+++ Mind Coach 3.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mind Coach 3.0
+//
+//  Created by Christine Agno on 7/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
